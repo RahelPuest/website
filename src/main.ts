c440446a1029ba29ace8910dd54160b23e130542
RahelPuest <axonpue@hotmail.de>
2026-01-13 21:30:21 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,62 @@
+import { Application, Assets, Rectangle, AnimatedSprite, TextureStyle } from "pixi.js";
+
+function lerp(a: number, b: number, t: number): number {
+  return a + (b - a) * t;
+}
+
+(async () => {
+  TextureStyle.defaultOptions.scaleMode = 'nearest'
+
+  const app = new Application();
+  await app.init({ background: "#1099bb", resizeTo: window });
+
+  document.getElementById("pixi-container")!.appendChild(app.canvas);
+
+  const sheet = await Assets.load("/assets/spritesheet.json");
+  const actor = new AnimatedSprite(sheet.animations["walk"]);
+  actor.anchor.set(0.5);
+  actor.position.set(app.screen.width / 2, app.screen.height / 2);
+  actor.animationSpeed = 0.15; // tweak
+  actor.stop(); // idle initially
+  app.stage.addChild(actor);
+
+  app.stage.eventMode = "static";
+  app.stage.hitArea = new Rectangle(0, 0, app.screen.width, app.screen.height);
+
+  let targetX = actor.x;
+  let targetY = actor.y;
+
+  app.stage.on("pointerdown", (e) => {
+    targetX = e.global.x;
+    targetY = e.global.y;
+  });
+
+  window.addEventListener("resize", () => {
+    app.stage.hitArea = new Rectangle(0, 0, app.screen.width, app.screen.height);
+  });
+
+  const speed = 400; // px per second
+  const stopEps = 0.5;
+
+  app.ticker.add((time) => {
+  const dt = time.deltaMS / 1000;
+    const step = speed * dt;
+
+    const dx = targetX - actor.x;
+    const dy = targetY - actor.y;
+    const dist = Math.hypot(dx, dy);
+
+    const moving = dist > stopEps;
+
+    if (moving) {
+      if (!actor.playing) actor.play();
+
+      const t = Math.min(1, step / dist);
+      actor.x = lerp(actor.x, targetX, t);
+      actor.y = lerp(actor.y, targetY, t);
+    } else {
+      if (actor.playing) actor.stop();
+      actor.position.set(targetX, targetY);
+    }
+  });
+})();
